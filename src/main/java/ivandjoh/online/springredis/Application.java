a3a3712967c367ce14b6f321175695cd83d22c5c
ivandjoh <ivandi.djoh@gmail.com>
2022-08-07 16:51:51 +0800

    mysql added

--- src/main/java/ivandjoh/online/springredis/Application.java
+++ src/main/java/ivandjoh/online/springredis/Application.java
@@ -1,9 +1,14 @@
 package ivandjoh.online.springredis;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@Configuration
+@PropertySource(value = "classpath:application.properties")
 public class Application {
 
 	public static void main(String[] args) {
